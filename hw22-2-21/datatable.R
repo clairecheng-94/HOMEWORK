6532d8cdcecbc5a4e9421adb9721747d5114e9f2
clairecheng-94 <76176353+clairecheng-94@users.noreply.github.com>
2021-02-23 21:44:10 -0500

    Update and rename R FILE PT.1 22-2-21 to hw22-2-21/datatable.R

--- hw22-2-21/datatable.R
+++ hw22-2-21/datatable.R
@@ -1,5 +1,5 @@
-matrix(data=rnorm(100), nrow=20, ncol=5)
-x<-matrix(data=rnorm(100), nrow=20, ncol=5)
+matrix(data=rnorm(1:100), nrow=20, ncol=5)
+x<-matrix(data=rnorm(1:100), nrow=20, ncol=5)
 X<-as.data.frame(x)
 outfolder<-"/home/cfc85413/temp_script2015"
 outfile<-paste(outfolder, "HW22-2-21". sep="/")
