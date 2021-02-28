21a34dbc2f1f4402898724256cf7fa01509abc43
clairecheng-94 <76176353+clairecheng-94@users.noreply.github.com>
2021-02-28 12:25:53 -0500

    Update datatable.R

--- hw22-2-21/datatable.R
+++ hw22-2-21/datatable.R
@@ -1,11 +1,12 @@
 ##WITHIN LINUX
 ##LOAD R
 
+seq(1:100)
 matrix(data=rnorm(1:100), nrow=20, ncol=5)
 x<-matrix(data=rnorm(1:100), nrow=20, ncol=5)
 X<-as.data.frame(x)
 outfolder<-"/home/cfc85413/temp_script2015"
-outfile<-paste(outfolder, "HW22-2-21". sep="/")
+outfile<-paste(outfolder, "HW22-2-21", sep="/")
 write.table(x=x, file=outfile, quote=FALSE, row.names=FALSE) 
 
 
